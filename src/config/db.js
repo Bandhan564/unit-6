a697af4f0d21dcb7f21fd12967d8855479fbfcc2
Bandhan Jayasingh <96365426+Bandhan564@users.noreply.github.com>
2022-06-10 16:29:32 +0530

    Delete db.js

--- src/config/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose')
-
-module.exports = ()=>{
-    return mongoose.connect("mongodb+srv://bandhan5660:<bandhan5660>@cluster0.8sydl.mongodb.net/?retryWrites=true&w=majority")
-}
